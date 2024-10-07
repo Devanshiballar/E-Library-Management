27bb946c542f473e55962e08ec7247016a714de4
devanshiballar <devanshiballar1611@gmail.com>
2024-10-08 02:20:07 +0530

    book

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use("/api/v1",userRoutes)
 app.use("/api/v1/book",bookRoutes)
 
 app.get("/",(req,res)=>{
-    res.send("<center><h1>E-Library Management Api</h1><br>Get Recipe Api <a href=https://github.com/Devanshiballar/Recipe-Management.git target=_blank>Repository :E-Library Management</a></center>")
+    res.send("<center><h1>E-Library Management Api</h1><br>Get Recipe Api <a href=https://github.com/Devanshiballar/E-Library-Management.git target=_blank>Repository :E-Library Management</a></center>")
   })
   
   app.listen(PORT, () => {
